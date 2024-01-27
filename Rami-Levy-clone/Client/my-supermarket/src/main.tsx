b8c275e1a06e32bc88259bf629baa768e14ae2b4
Lee_D <leegreat21@gmail.com>
2024-01-27 10:45:59 +0200

    home-page

--- Rami-Levy-clone/Client/my-supermarket/src/main.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/main.tsx
@@ -1,11 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
