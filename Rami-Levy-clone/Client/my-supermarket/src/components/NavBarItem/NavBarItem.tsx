c5878cf5eaee7ee1df03e085f003aabe9ab1bf4e
Lee_D <leegreat21@gmail.com>
2024-01-28 13:52:01 +0200

    building-navbar

--- Rami-Levy-clone/Client/my-supermarket/src/components/NavBarItem/NavBarItem.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/components/NavBarItem/NavBarItem.tsx
@@ -1,27 +1,32 @@
-import React from 'react';
+// NavbarItem.tsx
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import "./navbarItem.scss"; // Assuming you have a separate SCSS file for styling
 
-interface NavbarItemProps {
-  to: string;
+type NavbarItemProps = {
   label: string;
   iconSrc: string;
   activeIconSrc: string;
-}
+  to: string;
+};
 
-const NavbarItem: React.FC<NavbarItemProps> = ({ to, label, iconSrc, activeIconSrc }) => {
+const NavbarItem: React.FC<NavbarItemProps> = ({ label, iconSrc, activeIconSrc, to }) => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <li className="focus-item item d-xl-table-c position-relative">
-      <Link to={to} className="focus-item li-menu-title height-transition d-lg-block d-flex align-items-center align-top py-2 py-lg-3 w-100 border-radius-8 is-hover">
-        <img alt="" src={iconSrc} width="28" height="28" className="online-menu-img" />
-        <div className="li-menu-title-text d-flex justify-content-center align-items-center d-lg-block mobile-fix px-2 px-lg-0 py-1 line-height-1 pt-2">
-          <div className="d-name department px-1 w-100 align-self-center my-2 my-lg-0 s-text">
-            {label}
-          </div>
+    <li className={`nav-item${isActive ? ' active' : ''}`}>
+      <Link
+        to={to}
+        className={`nav-link${isActive ? ' active' : ''}`}
+        onMouseEnter={() => setIsActive(true)}
+        onMouseLeave={() => setIsActive(false)}
+        onClick={() => setIsActive(!isActive)}
+      >
+        <div className="navbar-item-content">
+          <img src={isActive ? activeIconSrc : iconSrc} />
+          <span>{label}</span>
         </div>
       </Link>
-      <ul className="focus-item ul-wrap-category border-radius-bottom-15 rl-scroll rl-group-wrap hidden row no-gutters pt-lg-3 px-lg-2 pb-2 categoies-list right-0 left-0 bg-gray-100 overflow-y-scroll-n">
-        {/* Submenu items go here */}
-      </ul>
     </li>
   );
 };
--- Rami-Levy-clone/Client/my-supermarket/src/components/Navbar/NavBar.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/components/Navbar/NavBar.tsx
@@ -1,8 +1,28 @@
+// NavBar.tsx
 import React from 'react';
 import NavbarItem from '../NavBarItem/NavBarItem';
-import "./navBar.scss"
+import "./navBar.scss";
 
 const NavBar: React.FC = () => {
+  const navbarItems = [
+    { label:"היסטוריית רכישה", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "מבצעים", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "פירות וירקות", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "חלב ביצים וסלטים", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "בשר ודגים", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "אורגני ובריאות", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "קפואים", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "שימורים בישול ואפיה", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "קטניות ודגנים", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "חטיפים ומתוקים", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "משקאות", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "חד-פעמי ומתכלה", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "אחזקת הבית ובע'ח", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    { label: "פארם ותינוקות", iconSrc: "/assets/icons/inactive/dairy-inactive.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/dairy" },
+    { label: "לחם ומאפים טריים", iconSrc: "/images/menu/blue-icons/49.svg", activeIconSrc: "/images/menu/colorful-icons/49.svg", to: "/fruits" },
+    // Add more items as needed
+  ];
+
   return (
     <div
       role="navigation"
@@ -11,21 +31,9 @@ const NavBar: React.FC = () => {
     >
       <div className="container-fluid">
         <ul id="main-menu" className="menu d-lg-flex showMenu justify-content-between ml-auto">
-          {/* Example usage of NavbarItem 1 */}
-          <NavbarItem
-            label="פירות וירקות"
-            iconSrc="/images/menu/blue-icons/49.svg"
-            activeIconSrc="/images/menu/colorful-icons/49.svg"
-            to="/fruits"
-          />
-
-          <NavbarItem
-            label="חלב ביצים וסלטים"
-            iconSrc="/assets/icons/inactive/dairy-inactive.svg"
-            activeIconSrc="/images/menu/colorful-icons/49.svg"
-            to="/dairy"
-          />
-          {/* Add more NavbarItem components as needed */}
+          {navbarItems.map((item, index) => (
+            <NavbarItem key={index} label={item.label} iconSrc={item.iconSrc} activeIconSrc={item.activeIconSrc} to={item.to} />
+          ))}
         </ul>
       </div>
     </div>
