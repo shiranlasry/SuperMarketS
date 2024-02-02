265bc4c1c6e67b63aaa0cb160cbe292d0d1c99f4
Lee_D <leegreat21@gmail.com>
2024-02-02 15:56:56 +0200

    form icons

---
+++ Rami-Levy-clone/Client/my-supermarket/src/assets/forms-svg/index.ts
@@ -0,0 +1,11 @@
+import login from "./login.svg"
+import register from "./register.svg"
+import showPass from "./showPass.svg"
+import dontShowPass from "./dontShowPass.svg.svg"
+import exit from "./exit.svg"
+
+
+
+
+
+export {login, register, showPass, dontShowPass, exit}
