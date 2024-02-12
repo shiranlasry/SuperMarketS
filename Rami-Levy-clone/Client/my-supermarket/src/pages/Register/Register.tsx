57b91addec69237dfc769ae3d30a833878c97910
Lee_D <leegreat21@gmail.com>
2024-02-12 15:39:14 +0200

    show-password-register

--- Rami-Levy-clone/Client/my-supermarket/src/pages/Register/Register.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/pages/Register/Register.tsx
@@ -11,19 +11,23 @@ interface RegisterProps {
 const Register: React.FC<RegisterProps> = ({ onClose }) => {
   const initialUserState: User = {
     user_id: null,
-    email: '',
-    id_number: '',
-    password: '',
-    confirm_password: '',
-    first_name: '',
-    last_name: '',
+    email: "",
+    id_number: "",
+    password: "",
+    confirm_password: "",
+    first_name: "",
+    last_name: "",
     role_id: 2,
-    addresses: []
+    addresses: [],
   };
 
   const [newUser, setNewUser] = useState<User>(initialUserState);
-  const [passwordValidation, setPasswordValidation] = useState<string | null>(null);
-  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState<string | null>(null);
+  const [passwordValidation, setPasswordValidation] = useState<string | null>(
+    null
+  );
+  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState<
+    string | null
+  >(null);
   const [IDValidation, setIDValidation] = useState<string | null>(null);
   const [emailValidation, setEmailValidation] = useState<string | null>(null);
   const [showPassword, setShowPassword] = useState<boolean>(false); // Add state for showing password
@@ -32,7 +36,9 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
   const navigate = useNavigate();
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >
   ) => {
     const { name, value } = e.target;
     setNewUser({ ...newUser, [name]: value });
@@ -60,7 +66,8 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
   };
 
   const validatePassword = (password: string) => {
-    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/;
+    const regex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/;
     if (newUser.password === newUser.confirm_password) {
       return null;
     }
@@ -70,7 +77,6 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
       return "הסיסמה חייבת להכיל לפחות 8 תווים, אות גדולה, אות קטנה, מספר ותו מיוחד";
     }
   };
-  
 
   const validateConfirmPassword = (confirmPassword: string) => {
     if (newUser.password === confirmPassword) {
@@ -90,7 +96,7 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
   };
 
   const toggleShowPassword = () => {
-    setShowPassword(prevState => !prevState); // Toggle the state
+    setShowPassword((prevState) => !prevState); // Toggle the state
   };
 
   const handelRegister = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -127,8 +133,92 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
 
   return (
     <div className="register-main">
-      <svg onClick={onClose} data-v-2d7301cc="" aria-label="סגור" xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" fill="none" viewBox="0 0 24 24" className="exit-reg-svg" stroke="currentColor"><path data-v-2d7301cc="" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M6 18L18 6M6 6l12 12"></path></svg>
-      <h1 className="register-title"><svg data-v-c9960dd8="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="22.58" height="24.52" viewBox="0 0 22.58 24.52" className="reg-svg"><defs data-v-c9960dd8=""><clipPath data-v-c9960dd8="" id="a" transform="translate(-1.62 -0.65)"><rect data-v-c9960dd8="" width="25.82" height="25.82" fill="none"></rect></clipPath></defs><circle data-v-c9960dd8="" cx="10.93" cy="6.15" r="5.65" fill="none" stroke="#0079f2" stroke-linecap="round" stroke-linejoin="round"></circle><path data-v-c9960dd8="" d="M12.55,24.67a14.74,14.74,0,0,0,9.71-3.89,2.21,2.21,0,0,0,.33-2.85,11.94,11.94,0,0,0-20.16.13,2.14,2.14,0,0,0,.41,2.71A14.68,14.68,0,0,0,12.55,24.67Z" transform="translate(-1.62 -0.65)" fill="none" stroke="#0079f2" stroke-linecap="round" stroke-linejoin="round"></path><line data-v-c9960dd8="" x1="17.22" y1="2.93" x2="22.08" y2="2.93" fill="none" stroke="#0079f2" stroke-linecap="round" stroke-linejoin="round"></line><line data-v-c9960dd8="" x1="19.65" y1="0.5" x2="19.65" y2="5.37" fill="none" stroke="#0079f2" stroke-linecap="round" stroke-linejoin="round"></line></svg>
+      <svg
+        onClick={onClose}
+        data-v-2d7301cc=""
+        aria-label="סגור"
+        xmlns="http://www.w3.org/2000/svg"
+        width="30px"
+        height="30px"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="exit-reg-svg"
+        stroke="currentColor"
+      >
+        <path
+          data-v-2d7301cc=""
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1"
+          d="M6 18L18 6M6 6l12 12"
+        ></path>
+      </svg>
+      <h1 className="register-title">
+        <svg
+          data-v-c9960dd8=""
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="22.58"
+          height="24.52"
+          viewBox="0 0 22.58 24.52"
+          className="reg-svg"
+        >
+          <defs data-v-c9960dd8="">
+            <clipPath
+              data-v-c9960dd8=""
+              id="a"
+              transform="translate(-1.62 -0.65)"
+            >
+              <rect
+                data-v-c9960dd8=""
+                width="25.82"
+                height="25.82"
+                fill="none"
+              ></rect>
+            </clipPath>
+          </defs>
+          <circle
+            data-v-c9960dd8=""
+            cx="10.93"
+            cy="6.15"
+            r="5.65"
+            fill="none"
+            stroke="#0079f2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></circle>
+          <path
+            data-v-c9960dd8=""
+            d="M12.55,24.67a14.74,14.74,0,0,0,9.71-3.89,2.21,2.21,0,0,0,.33-2.85,11.94,11.94,0,0,0-20.16.13,2.14,2.14,0,0,0,.41,2.71A14.68,14.68,0,0,0,12.55,24.67Z"
+            transform="translate(-1.62 -0.65)"
+            fill="none"
+            stroke="#0079f2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <line
+            data-v-c9960dd8=""
+            x1="17.22"
+            y1="2.93"
+            x2="22.08"
+            y2="2.93"
+            fill="none"
+            stroke="#0079f2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></line>
+          <line
+            data-v-c9960dd8=""
+            x1="19.65"
+            y1="0.5"
+            x2="19.65"
+            y2="5.37"
+            fill="none"
+            stroke="#0079f2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></line>
+        </svg>
         הרשמה
       </h1>
       <form className="register-form" onSubmit={handelRegister}>
@@ -160,21 +250,147 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
           required
           title="הכנס כתובת מייל נכונה"
         />
+
         {emailValidation && (
           <div className="error-message">{emailValidation}</div>
         )}
-        <input
-          type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
-          placeholder="סיסמה*"
-          name="password"
-          id="password"
-          value={newUser.password}
-          onChange={handleInputChange}
-          required
-        />
-        {passwordValidation && (
-          <div className="error-message">{passwordValidation}</div>
-        )}
+        <div className="reg-pass-wrapper">
+          <button
+            className="show-password-btn"
+            type="button"
+            onClick={toggleShowPassword}
+          >
+            {showPassword ? (
+              <svg
+                data-v-91686126=""
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="25.47"
+                height="8.57"
+                viewBox="0 0 25.47 8.57"
+                class="stroke-width-1"
+              >
+                <defs data-v-91686126="">
+                  <clipPath
+                    data-v-91686126=""
+                    id="a"
+                    transform="translate(-1.7 -10.15)"
+                  >
+                    <rect
+                      data-v-91686126=""
+                      width="28.87"
+                      height="28.87"
+                      fill="none"
+                    ></rect>
+                  </clipPath>
+                </defs>
+                <path
+                  data-v-91686126=""
+                  d="M2.2,17C9,8.52,19.91,8.52,26.67,17h0"
+                  transform="translate(-1.7 -10.15)"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <circle
+                  data-v-91686126=""
+                  cx="12.74"
+                  cy="4.29"
+                  r="3.79"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></circle>
+              </svg>
+            ) : (
+              <svg
+                data-v-91686126=""
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                width="25.47"
+                height="14.73"
+                viewBox="0 0 25.47 14.73"
+                class="stroke-width-1"
+              >
+                <defs data-v-91686126="">
+                  <clipPath
+                    data-v-91686126=""
+                    id="a"
+                    transform="translate(-1.7 -7.07)"
+                  >
+                    <rect
+                      data-v-91686126=""
+                      width="28.87"
+                      height="28.87"
+                      fill="none"
+                    ></rect>
+                  </clipPath>
+                </defs>
+                <path
+                  data-v-91686126=""
+                  d="M11.07,11.07A16.46,16.46,0,0,0,2.2,17"
+                  transform="translate(-1.7 -7.07)"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  data-v-91686126=""
+                  d="M11.77,11.77a3.73,3.73,0,0,0-1.12,2.67A3.77,3.77,0,0,0,17.1,17.1"
+                  transform="translate(-1.7 -7.07)"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <line
+                  data-v-91686126=""
+                  x1="19.6"
+                  y1="14.23"
+                  x2="5.87"
+                  y2="0.5"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></line>
+                <path
+                  data-v-91686126=""
+                  d="M18.07,15.41a3.45,3.45,0,0,0,.15-1,3.79,3.79,0,0,0-3.79-3.79,3.45,3.45,0,0,0-1,.15"
+                  transform="translate(-1.7 -7.07)"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  data-v-91686126=""
+                  d="M26.67,17c-3.65-4.59-8.52-6.69-13.3-6.32"
+                  transform="translate(-1.7 -7.07)"
+                  fill="none"
+                  stroke="#0079f2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            )}
+          </button>
+          <input
+            type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
+            placeholder="סיסמה*"
+            name="password"
+            id="password"
+            value={newUser.password}
+            onChange={handleInputChange}
+            required
+          />
+          {passwordValidation && (
+            <div className="error-message">{passwordValidation}</div>
+          )}
+        </div>
         <input
           type={showPassword ? "text" : "password"} // Toggle input type based on showPassword state
           placeholder="אישור סיסמה*"
@@ -186,9 +402,7 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
         {confirmPasswordValidation && (
           <div className="error-message">{confirmPasswordValidation}</div>
         )}
-        <button className="show-password-btn" type="button" onClick={toggleShowPassword}>
-          {showPassword ? "הסתר סיסמה" : "הצג סיסמה"}
-        </button>
+
         <input
           type="text"
           placeholder="תעודת זהות*"
@@ -198,32 +412,27 @@ const Register: React.FC<RegisterProps> = ({ onClose }) => {
           onChange={handleInputChange}
           required
         />
-        {IDValidation && (
-          <div className="error-message">{IDValidation}</div>
-        )}
+        {IDValidation && <div className="error-message">{IDValidation}</div>}
         <div className="reg-disclaimers">
-        <p>
-          הנני מאשר/ת את תקנון אתר רמי לוי באינטרנט המחודש ואת הצטרפותי ללא עלות
-          למועדון לקוחות רמי לוי בכפוף לתקנון המועדון *
-        </p>
-        <p>
-          הנני מאשר/ת לקבל הטבות, מבצעים, עדכונים והצעות למוצרים ושירותים מקבוצת
-          רמי לוי באמצעי התקשורת שמסרתי לקבוצה. ידוע לי כי אוכל לחזור בי מהסכמתי
-          בכל עת.
-        </p>
-      </div>
-      <div>
-      <button className="register-cancel" onClick={onClose}>
-          ביטול
-        </button>
-        <button className="register-btn" type="submit">
-          קחו אותי לסופר!
-        </button>
-        
-      </div>
+          <p>
+            הנני מאשר/ת את תקנון אתר רמי לוי באינטרנט המחודש ואת הצטרפותי ללא
+            עלות למועדון לקוחות רמי לוי בכפוף לתקנון המועדון *
+          </p>
+          <p>
+            הנני מאשר/ת לקבל הטבות, מבצעים, עדכונים והצעות למוצרים ושירותים
+            מקבוצת רמי לוי באמצעי התקשורת שמסרתי לקבוצה. ידוע לי כי אוכל לחזור
+            בי מהסכמתי בכל עת.
+          </p>
+        </div>
+        <div>
+          <button className="register-cancel" onClick={onClose}>
+            ביטול
+          </button>
+          <button className="register-btn" type="submit">
+            קחו אותי לסופר!
+          </button>
+        </div>
       </form>
-      
-   
     </div>
   );
 };
