40cf4437aa280a6578b89bd6bfe0d89eba64554a
Lee_D <leegreat21@gmail.com>
2024-02-23 15:32:34 +0200

    UI-header-refinement

--- Rami-Levy-clone/Client/my-supermarket/src/components/Header/Header.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/components/Header/Header.tsx
@@ -26,47 +26,31 @@ const Header = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
+  // Toggle the menu state
   const toggleMenu = () => {
-    // Toggle the menu state regardless of user login status
     setIsMenuOpen((prevState) => !prevState);
   };
 
-  // Open the menu only when the user clicks on their name
-  // const handleMenuClick = () => {
-  //   // Only open the menu if the user is logged in
-  //   if (loggedInUser) {
-  //     setIsMenuOpen(true);
-  //   }
-  // };
-
-  // check if there is user loggedin on cookie token
+  // Check if there is a logged-in user based on cookie token
   useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (!event.target) return;
-
-      const target = event.target as HTMLElement;
-      const menu = document.querySelector(".greet-user");
-      if (!menu || menu.contains(target)) return;
-
-      setIsMenuOpen(false);
-    };
-
-    document.addEventListener("click", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("click", handleOutsideClick);
-    };
+    if (!loggedInUser) {
+      dispatch(getUserFromTokenApi());
+    }
   }, []);
 
+  // Close the login modal when the register modal is shown
   useEffect(() => {
     if (showRegisterModal) {
       setShowLoginModal(false);
     }
   }, [showRegisterModal]);
 
+  // Close the login modal
   const handleCloseLogin = () => {
     setShowLoginModal(false);
   };
+
+  // Close the register modal and show the login modal
   const handleCloseRegister = () => {
     setShowRegisterModal(false);
     setShowLoginModal(true);
@@ -75,71 +59,71 @@ const Header = () => {
   return (
     <div className="sub-categories-main">
       <button className="to-main-navBar" onClick={() => navigate("/")}>
-        {" "}
-        {/* Add onClick handler */}
-        <img className="rami-online" src={Logo} />
+        <img className="rami-online" src={Logo} alt="Rami Levy Online" />
       </button>
       <button className="to-shoppin-navBar">
-        <img className="rami-shopping" src={Shopping} />
+        <img
+          className="rami-shopping"
+          src={Shopping}
+          alt="Rami Levy Shopping"
+        />
       </button>
       <SearchBar />
       <NightMode />
       <button className="access">הצהרת נגישות</button>
       {!loggedInUser && (
-        <>
-          <button
-            className="hp-loginBtn"
-            onClick={() => setShowLoginModal(true)}
-            aria-haspopup={true}
-            aria-expanded={showLoginModal}
-            aria-controls="modal-login"
+        <button
+          className="hp-loginBtn"
+          onClick={() => setShowLoginModal(true)}
+          aria-haspopup={true}
+          aria-expanded={showLoginModal}
+          aria-controls="modal-login"
+        >
+          <svg
+            data-v-c9960dd8=""
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="21.84"
+            height="24.52"
+            viewBox="0 0 21.84 24.52"
+            className="hp-loginSvg"
           >
-            <svg
-              data-v-c9960dd8=""
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              width="21.84"
-              height="24.52"
-              viewBox="0 0 21.84 24.52"
-              className="hp-loginSvg"
-            >
-              <defs data-v-c9960dd8="">
-                <clipPath
-                  data-v-c9960dd8=""
-                  id="a"
-                  transform="translate(-1.99 -0.65)"
-                >
-                  <rect
-                    data-v-c9960dd8=""
-                    width="25.82"
-                    height="25.82"
-                    fill="none"
-                  ></rect>
-                </clipPath>
-              </defs>
-              <circle
+            <defs data-v-c9960dd8="">
+              <clipPath
                 data-v-c9960dd8=""
-                cx="10.93"
-                cy="6.15"
-                r="5.65"
-                fill="none"
-                stroke="#0079f2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></circle>
-              <path
-                data-v-c9960dd8=""
-                d="M12.92,24.67a14.74,14.74,0,0,0,9.71-3.89A2.22,2.22,0,0,0,23,17.93a11.94,11.94,0,0,0-20.16.13,2.14,2.14,0,0,0,.41,2.71A14.68,14.68,0,0,0,12.92,24.67Z"
+                id="a"
                 transform="translate(-1.99 -0.65)"
-                fill="none"
-                stroke="#0079f2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
-            <span className="login-title"> התחברות</span>
-          </button>
-        </>
+              >
+                <rect
+                  data-v-c9960dd8=""
+                  width="25.82"
+                  height="25.82"
+                  fill="none"
+                ></rect>
+              </clipPath>
+            </defs>
+            <circle
+              data-v-c9960dd8=""
+              cx="10.93"
+              cy="6.15"
+              r="5.65"
+              fill="none"
+              stroke="#0079f2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></circle>
+            <path
+              data-v-c9960dd8=""
+              d="M12.92,24.67a14.74,14.74,0,0,0,9.71-3.89A2.22,2.22,0,0,0,23,17.93a11.94,11.94,0,0,0-20.16.13,2.14,2.14,0,0,0,.41,2.71A14.68,14.68,0,0,0,12.92,24.67Z"
+              transform="translate(-1.99 -0.65)"
+              fill="none"
+              stroke="#0079f2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+          <span className="login-title"> התחברות</span>
+        </button>
       )}
 
       {loggedInUser && (
@@ -180,8 +164,8 @@ const Header = () => {
               r="5.65"
               fill="none"
               stroke="#0079f2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></circle>
             <path
               data-v-c9960dd8=""
@@ -189,8 +173,8 @@ const Header = () => {
               transform="translate(-1.99 -0.65)"
               fill="none"
               stroke="#0079f2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
           </svg>
           {loggedInUser.first_name}
