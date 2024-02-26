4be18c1f8b3e5a7bf062e63cc9fe3d5d45cbe25b
Lee_D <leegreat21@gmail.com>
2024-02-26 17:52:39 +0200

    new-password-UI

--- Rami-Levy-clone/Client/my-supermarket/src/pages/PersonalProfil/UpdateUserPassword/UpdateUserPassword.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/pages/PersonalProfil/UpdateUserPassword/UpdateUserPassword.tsx
@@ -1,80 +1,164 @@
-import React, { useState } from 'react'
-import './UpdateUserPassword.scss'
-import { User } from '../../../rami-types';
-import { useAppDispatch } from '../../../app/hook';
-import { updateUserPasswordApi } from '../../../features/logged_in_user/loggedInUserAPI';
-
+import React, { useState } from "react";
+import "./UpdateUserPassword.scss";
+import { User } from "../../../rami-types";
+import { useAppDispatch } from "../../../app/hook";
+import { updateUserPasswordApi } from "../../../features/logged_in_user/loggedInUserAPI";
 
 type UserProps = {
-    user: User;
-    onClose: () => void;
-  };
+  user: User;
+  onClose: () => void;
+};
 const UpdateUserPassword: React.FC<UserProps> = ({ user, onClose }) => {
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [oldPassword, setOldPassword] = useState("");
-    const [passwordValidation, setPasswordValidation] = useState<string | null>(null);
-    const [confirmPasswordValidation, setConfirmPasswordValidation] = useState<string | null>(null);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [passwordValidation, setPasswordValidation] = useState<string | null>(
+    null
+  );
+  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState<
+    string | null
+  >(null);
 
-    const dispatch = useAppDispatch();
-    const validatePassword = (password: string) => {
-        const regex =
-          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/;
-        if (user.password === user.confirm_password) {
-          return null;
-        }
-        if (regex.test(password)) {
-          return null;
-        } else {
-          return "הסיסמה חייבת להכיל לפחות 8 תווים, אות גדולה, אות קטנה, מספר ותו מיוחד";
-        }
-      };
-      const validateConfirmPassword = (confirmPassword: string) => {
-        if (password === confirmPassword) {
-          return null;
-        } else {
-          return "הסיסמאות אינן תואמות";
-        }
-      };
-      const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        if (name === "password") {
-          setPassword(value);
-          setPasswordValidation(validatePassword(value)); // Validate password input
-        } else if (name === "confirmPassword") {
-          setConfirmPassword(value);
-          setConfirmPasswordValidation(validateConfirmPassword(value)); // Validate confirm password input
-        }
-        else if (name === "oldPassword") {
-            setOldPassword(value);
-          }
-      };
+  const dispatch = useAppDispatch();
+  const validatePassword = (password: string) => {
+    const regex =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?!.*\s).{8,}$/;
+    if (user.password === user.confirm_password) {
+      return null;
+    }
+    if (regex.test(password)) {
+      return null;
+    } else {
+      return "הסיסמה חייבת להכיל לפחות 8 תווים, אות גדולה, אות קטנה, מספר ותו מיוחד";
+    }
+  };
+  const validateConfirmPassword = (confirmPassword: string) => {
+    if (password === confirmPassword) {
+      return null;
+    } else {
+      return "הסיסמאות אינן תואמות";
+    }
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "password") {
+      setPassword(value);
+      setPasswordValidation(validatePassword(value)); // Validate password input
+    } else if (name === "confirmPassword") {
+      setConfirmPassword(value);
+      setConfirmPasswordValidation(validateConfirmPassword(value)); // Validate confirm password input
+    } else if (name === "oldPassword") {
+      setOldPassword(value);
+    }
+  };
 
-      const handleUpdatePassword = () => {
-        debugger;
-        if (passwordValidation || confirmPasswordValidation || !oldPassword || !password || !confirmPassword ||!user.user_id) {
-            alert("אחד או יותר מהשדות אינם תקינים");
-          return;
-        }
-       dispatch(updateUserPasswordApi({ user_id: user.user_id, old_password: oldPassword, new_password: password }));
-        onClose();
-
-      }
+  const handleUpdatePassword = () => {
+    if (
+      passwordValidation ||
+      confirmPasswordValidation ||
+      !oldPassword ||
+      !password ||
+      !confirmPassword ||
+      !user.user_id
+    ) {
+      alert("אחד או יותר מהשדות אינם תקינים");
+      return;
+    }
+    dispatch(
+      updateUserPasswordApi({
+        user_id: user.user_id,
+        old_password: oldPassword,
+        new_password: password,
+      })
+    );
+    onClose();
+  };
   return (
-    <div className='update-user-password-container'>
-        <h1>עדכון סיסמא</h1>
-        <div className='update-user-password-form'>
-        <input type="password" placeholder="סיסמא ישנה" name="oldPassword" onChange={handleInputChange} />
-        <input type="password" placeholder="סיסמא חדשה" name="password" onChange={handleInputChange} />
-        {passwordValidation && <span>{passwordValidation}</span>}
-        <input type="password" placeholder="אימות סיסמא" name="confirmPassword" onChange={handleInputChange} />
-        {confirmPasswordValidation && <span>{confirmPasswordValidation}</span>}
-        <button onClick={handleUpdatePassword}>שמירה</button>
-        <button onClick={onClose}>ביטול</button>
-        </div>
-      
-    </div>
-  )
-}
+    <div className="update-user-password-container">
+      <div className="modal-header">
+        <h3 className="new-password-title">
+          <svg
+            data-v-19c2d99e=""
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="30.01"
+            viewBox="0 0 21 30.01"
+            className="lock-svg"
+          >
+            <rect
+              data-v-19c2d99e=""
+              x="0.5"
+              y="9.51"
+              width="20"
+              height="20"
+              rx="3.05"
+              fill="none"
+              stroke="#0079f2"
+              stroke-miterlimit="10"
+            ></rect>
+            <path
+              data-v-19c2d99e=""
+              d="M16.27,9.17V6.27A5.77,5.77,0,0,0,10.5.5h0A5.77,5.77,0,0,0,4.73,6.27h0v2.9"
+              fill="none"
+              stroke="#0079f2"
+              stroke-miterlimit="10"
+            ></path>
+            <path
+              data-v-19c2d99e=""
+              d="M13.47,17.12a3,3,0,1,0-4,2.77v3.92a1,1,0,0,0,1.05,1.05h0a1,1,0,0,0,1.05-1.05h0V19.89A3,3,0,0,0,13.47,17.12Z"
+              fill="none"
+              stroke="#0079f2"
+              stroke-miterlimit="10"
+            ></path>
+          </svg>{" "}
+          סיסמה חדשה
+        </h3>
+        <button
+          type="button"
+          className="new-password-closeBtn"
+          onClick={onClose}
+        >
+          <span>&times;</span>
+        </button>
+      </div>
 
-export default UpdateUserPassword
+      <div className="update-user-password-form">
+        <input
+          type="password"
+          placeholder="סיסמה קודמת*"
+          name="oldPassword"
+          className="new-password-input"
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="סיסמה חדשה*"
+          name="password"
+          className="new-password-input"
+          onChange={handleInputChange}
+        />
+        {passwordValidation && <span>{passwordValidation}</span>}
+        <input
+          type="password"
+          placeholder="אישור סיסמה*"
+          name="confirmPassword"
+          className="new-password-input"
+          onChange={handleInputChange}
+        />
+        <div className="new-password-btns">
+          {confirmPasswordValidation && (
+            <span>{confirmPasswordValidation}</span>
+          )}
+          <button className="new-pass-cancel" onClick={onClose}>
+            ביטול
+          </button>
+          <button className="new-pass-save" onClick={handleUpdatePassword}>
+            שמירה
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateUserPassword;
