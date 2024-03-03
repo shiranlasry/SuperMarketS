1b4d11acf03fba01087da3beac006ddd1b0b0636
Lee_D <leegreat21@gmail.com>
2024-03-03 19:49:13 +0200

    shopping-cart-layout

--- Rami-Levy-clone/Client/my-supermarket/src/views/layouts/Layout.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/views/layouts/Layout.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import "./layout.scss";
 
 const Layout = () => {
   return (
     <>
+      <>cart</>
+      {/* // conditional rendering when user loggerd in */}
       <Header />
       <main>
         <Outlet />
