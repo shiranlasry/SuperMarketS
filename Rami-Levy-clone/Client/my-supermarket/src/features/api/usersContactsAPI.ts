af1139b12af7b19e1645748fca0059114b0dbb4c
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2024-03-10 02:21:54 +0200

    debugger

--- Rami-Levy-clone/Client/my-supermarket/src/features/api/usersContactsAPI.ts
+++ Rami-Levy-clone/Client/my-supermarket/src/features/api/usersContactsAPI.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 
 export const addNewUserContactAPI = async ( contact_name: string, contact_phone_number: string) => {
     try {
-        debugger;
         const response = await axios.post('/api/users-contacts/add-new-user-contact', {  contact_name, contact_phone_number  });
         const { ok, results } = response.data;
         if (!ok) {
