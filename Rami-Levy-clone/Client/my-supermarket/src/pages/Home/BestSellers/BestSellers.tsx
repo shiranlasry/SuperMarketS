b5e763ba0743e5db3d99a695b83cb6f88c89e0a8
Lee_D <leegreat21@gmail.com>
2024-03-19 10:16:47 +0200

    hp-feed-grid

--- Rami-Levy-clone/Client/my-supermarket/src/pages/Home/BestSellers/BestSellers.tsx
+++ Rami-Levy-clone/Client/my-supermarket/src/pages/Home/BestSellers/BestSellers.tsx
@@ -1,49 +1,50 @@
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../app/hook';
-import { allOrdersSalesDetailsSelector } from '../../../features/orders/ordersSlice';
-import { getAllOrdersSalesDetailsAPI } from '../../../features/orders/ordersAPI';
-import { productsSelector } from '../../../features/products/productsSlice';
-import { getAllProductsApi } from '../../../features/products/productsAPI';
-import ProductCard from '../../../components/ProductCard/ProductCard';
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../app/hook";
+import { allOrdersSalesDetailsSelector } from "../../../features/orders/ordersSlice";
+import { getAllOrdersSalesDetailsAPI } from "../../../features/orders/ordersAPI";
+import { productsSelector } from "../../../features/products/productsSlice";
+import { getAllProductsApi } from "../../../features/products/productsAPI";
+import ProductCard from "../../../components/ProductCard/ProductCard";
+import "./best-sellers.scss";
 
 const BestSellers = () => {
-    const allProductsSaleAmount = useAppSelector(allOrdersSalesDetailsSelector);
-    const allProducts = useAppSelector(productsSelector);
-    const dispatch = useAppDispatch();
+  const allProductsSaleAmount = useAppSelector(allOrdersSalesDetailsSelector);
+  const allProducts = useAppSelector(productsSelector);
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        if (!allProductsSaleAmount) {
-            dispatch(getAllOrdersSalesDetailsAPI());
-        }
-        if (!allProducts) {
-             dispatch(getAllProductsApi());
-        }
-    }, []);
+  useEffect(() => {
+    if (!allProductsSaleAmount) {
+      dispatch(getAllOrdersSalesDetailsAPI());
+    }
+    if (!allProducts) {
+      dispatch(getAllProductsApi());
+    }
+  }, []);
 
-    useEffect(() => {
-        console.log(allProductsSaleAmount);
-    }, [allProductsSaleAmount]);
+  useEffect(() => {
+    console.log(allProductsSaleAmount);
+  }, [allProductsSaleAmount]);
 
-    // Sort the products by total_quantity
-    const sortedProducts = allProductsSaleAmount
-        ? allProductsSaleAmount.slice().sort((a, b) => (b.total_quantity || 0) - (a.total_quantity || 0))
-        : [];
+  // Sort the products by total_quantity
+  const sortedProducts = allProductsSaleAmount
+    ? allProductsSaleAmount
+        .slice()
+        .sort((a, b) => (b.total_quantity || 0) - (a.total_quantity || 0))
+    : [];
 
-    // Get the top 20 products
-    const top20Products = sortedProducts.slice(0, 20);
+  // Get the top 20 products
+  const top20Products = sortedProducts.slice(0, 20);
 
-    return (
-        <div>
-            <h1>המוצרים הנמכרים ביותר</h1>
-            
-                {top20Products.map(product => (
-                 
-                        <ProductCard product={product} />
-                   
-                ))}
-           
-        </div>
-    );
-}
+  return (
+    <div className="hp-feed-main">
+      <h1 className="hp-feed-title">המוצרים הנמכרים ביותר</h1>
+      <div className="best-seller-prods">
+        {top20Products.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default BestSellers;
